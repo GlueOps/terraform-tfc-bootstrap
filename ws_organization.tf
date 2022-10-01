844aa4e04c75d3cefafefb8ffe875c91530659ba
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-09-30 20:46:07 -0700

    feat: adding gcp and aws vcs repos

--- ws_organization.tf
+++ ws_organization.tf
@@ -5,7 +5,7 @@ resource "tfe_workspace" "gcp-organization" {
   description       = "Terraform for managing the GCP organization and all associated projects."
   working_directory = "organization/tf"
   vcs_repo {
-    identifier     = "${var.githhub_org_name}/${var.vcs_repo}"
+    identifier     = "${var.githhub_org_name}/${var.gcp_vcs_repo}"
     branch         = "main"
     oauth_token_id = var.github_token_id
 
@@ -22,7 +22,7 @@ resource "tfe_workspace" "aws-organization" {
   description       = "Terraform for managing the AWS organization and all associated accounts."
   working_directory = "organization/tf"
   vcs_repo {
-    identifier     = "${var.githhub_org_name}/${var.vcs_repo}"
+    identifier     = "${var.githhub_org_name}/${var.aws_vcs_repo}"
     branch         = "main"
     oauth_token_id = var.github_token_id
 
