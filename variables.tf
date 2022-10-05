5082282d48fd2eb5ed869b27603f3ad4f99df5db
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-10-04 18:28:42 -0700

    feat: adding workspace for cloudflare

--- variables.tf
+++ variables.tf
@@ -7,6 +7,8 @@ variable "github_token_id" {}
 variable "githhub_org_name" {}
 variable "gcp_vcs_repo" { default = "gcp-infrastructure" }
 variable "aws_vcs_repo" { default = "aws-infrastructure" }
+variable "cloudflare_vcs_repo"  { default = "cloudflare-infrastructure" }
+variable "cloudflare_enabled" { default = false }
 variable "terraform_version" { default = "1.2.9" }
 variable "clouds" { default = ["gcp"] }
 variable "terraform_cloud_repo" { default = "terraform-cloud" }
