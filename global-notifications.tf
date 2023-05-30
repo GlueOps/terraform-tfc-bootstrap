3437558df4905af96f7a65f1c45f8e7e3ae23de7
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-29 21:13:39 -0700

    Update global-notifications.tf

--- global-notifications.tf
+++ global-notifications.tf
@@ -1,19 +1,19 @@
 
-locals {
-  trigger = {
-    gcp = contains(var.clouds, "gcp") ? tfe_workspace.gcp-organization[0].id : 0
-    aws = contains(var.clouds, "aws") ? tfe_workspace.aws-organization[0].id : 0
-  }
-  triggers = [for k, v in local.trigger : v if v != 0]
-}
+# locals {
+#   trigger = {
+#     gcp = contains(var.clouds, "gcp") ? tfe_workspace.gcp-organization[0].id : 0
+#     aws = contains(var.clouds, "aws") ? tfe_workspace.aws-organization[0].id : 0
+#   }
+#   triggers = [for k, v in local.trigger : v if v != 0]
+# }
 
-resource "tfe_notification_configuration" "slack" {
-  for_each         = toset(concat([tfe_workspace.terraform-cloud.id],local.triggers))
-  name             = "needs_attention"
-  enabled          = true
-  destination_type = "slack"
-  url              = "https://hooks.slack.com/services/${var.slack_token}"
-  triggers         = var.notification_triggers
-  workspace_id     = each.key
-}
+# resource "tfe_notification_configuration" "slack" {
+#   for_each         = toset(concat([tfe_workspace.terraform-cloud.id],local.triggers))
+#   name             = "needs_attention"
+#   enabled          = true
+#   destination_type = "slack"
+#   url              = "https://hooks.slack.com/services/${var.slack_token}"
+#   triggers         = var.notification_triggers
+#   workspace_id     = each.key
+# }
 
